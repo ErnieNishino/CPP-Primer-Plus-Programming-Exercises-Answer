5764f40f94777b4311dac97c1e8154114e0d65ad
ErnieNishino <108204432+ErnieNishino@users.noreply.github.com>
2022-08-02 21:31:40 +0800

    Add files via upload

---
+++ chapter_2.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+// void print_1();
+// void print_2();
+// double cel2fah(double cel);
+// double ly2au(double ly);
+// void print_time(int hour, int minutes);
+
+int main() {
+    using namespace std;
+
+    // Problem 1
+    // cout << "name" << "address" << endl;
+
+    // Problem 2
+    // int lon, m;
+    // cout << "Please enter a number with long" << endl;
+    // cin >> lon;
+    // m = lon * 220;
+    // cout << m << endl;
+
+    // Problem 3
+    // Please unanotation function print_1 and print_2 
+    // print_1();
+    // print_1();
+    // print_2();
+    // print_2();
+
+    // Problem 4
+    // int year, month;
+    // cin >> year;
+    // month = year * 12;
+    // cout << month;
+
+    // Problem 5
+    // Please unanotation function cel2fah
+    // double celsius, fahrenheit;
+    // cout << "Please Enter a Celsius Value: ";
+    // cin >> celsius;
+    // fahrenheit = cel2fah(celsius);
+    // cout << celsius << " degrees Celsius is " << fahrenheit << " degrees Fahrenheit" << endl;
+
+    // Problem 6
+    // Please unanotation function cel2fah
+    // double ly, au;
+    // cout << "Enter the number of light years: ";
+    // cin >> ly;
+    // au = ly2au(ly);
+    // cout << ly << " light years = " << au << " astronomical units." << endl;
+
+    // Problem 7
+    // int hours, minutes;
+    // cout << "Enter the number of hours: ";
+    // cin >> hours;
+    // cout << "Enter the number of minutes: ";
+    // cin >> minutes;
+    // print_time(hours, minutes);
+
+    return 0;
+}
+
+// void print_1() {
+//     using namespace std;
+//     cout << "Three blind mice" << endl;
+// }
+
+// void print_2() {
+//     using namespace std;
+//     cout << "So how they run" << endl;
+// }
+
+// double cel2fah(double cel) {
+//     return cel * 1.8 + 32.0;
+// }
+
+// double ly2au(double ly) {
+//     return ly * 63240;
+// }
+
+// void print_time(int hour, int minutes) {
+//     using namespace std;
+//     cout << "Time: " << hour << ":" << minutes;
+// }
